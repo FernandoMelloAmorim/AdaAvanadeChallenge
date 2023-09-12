6a9800042b276d9d3ac3b87930d51f4aaeb94a42
FernandoMelloAmorim <fmelloamorim@gmail.com>
2023-09-12 12:33:17 -0300

    Project created

---
+++ src/main/java/com/SpringWarriors/AdaAvanadeChallenge/AdaAvanadeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.SpringWarriors.AdaAvanadeChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaAvanadeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaAvanadeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringWarriors/AdaAvanadeChallenge/AdaAvanadeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringWarriors.AdaAvanadeChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaAvanadeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
